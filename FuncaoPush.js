768dfebc4dc25a4f0198c080db65211417738689
anacperdigao <anacarolinaperdigao1@gmail.com>
2022-09-24 21:14:42 -0300

    Funçao Push

---
+++ FuncaoPush.js
@@ -0,0 +1,11 @@
+const nomes = ['Eduardo', 'Maria', 'Joana'];
+nomes.push('João');
+nomes.push('Ana');
+
+console.log(nomes)
+
+/*
+Info: Start process (21:14:15)
+[ 'Eduardo', 'Maria', 'Joana', 'João', 'Ana' ]
+Info: End process (21:14:15)
+*/
